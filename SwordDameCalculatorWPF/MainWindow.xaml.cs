6af96524c94b95fedfc80e9610bb052d2bae1889
Bartek <bartlomiej.kacki@gmail.com>
2024-06-02 20:00:51 +0200

    Add project files.

---
+++ SwordDameCalculatorWPF/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SwordDameCalculatorWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Random random = new Random();
+        SwordDame swordDamage = new SwordDame();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            swordDamage.SetMagic(false);
+            swordDamage.SetFlaming(false);
+            RollDice();
+        }
+
+        private void RollDice()
+        {
+            swordDamage.Roll = random.Next(1, 7) + random.Next(1, 7) + random.Next(1, 7);
+            swordDamage.SetFlaming(flaming.IsChecked.Value);
+            swordDamage.SetMagic(magic.IsChecked.Value);
+            DisplayDamage();
+        }
+
+        private void DisplayDamage()
+        {
+            damage.Text = "Rzut: " + swordDamage.Roll + ", punkty obrażeń: " + swordDamage.Damage;
+        }
+
+        private void Flaming_Checked(object sender, RoutedEventArgs e)
+        {
+            swordDamage.SetFlaming(true);
+            DisplayDamage();
+        }
+
+        private void Flaming_Unchecked(object sender, RoutedEventArgs e)
+        {
+            swordDamage.SetFlaming(false);
+            DisplayDamage();
+        }
+
+        private void Magic_Unchecked(object sender, RoutedEventArgs e)
+        {
+            swordDamage.SetMagic(false);
+            DisplayDamage();
+        }
+
+        private void Magic_Checked(object sender, RoutedEventArgs e)
+        {
+            swordDamage.SetMagic(true);
+            DisplayDamage();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            RollDice();
+        }
+    }
+}
---
+++ SwordDameCalculatorWPF/SwordDame.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace SwordDameCalculatorWPF
+{
+    internal class SwordDame
+    {
+        public const int BASE_DAMAGE = 3;
+        public const int FLAME_DAMAGE = 2;
+
+        public int Roll;
+        private decimal MagicMultiplier = 1M;
+        private int FlamingDamage = 0;
+        public int Damage;
+
+        private void CalculateDamage()
+        {
+            Damage = (int)(Roll * MagicMultiplier) + BASE_DAMAGE + FlamingDamage;
+            Debug.WriteLine($"Po wykonaniu CalculateDamage: {Damage} (rzut: {Roll})");
+        }
+
+        public void SetMagic(bool isMagic)
+        {
+            if (isMagic)
+                MagicMultiplier = 1.75M;
+            else
+                MagicMultiplier = 1M;
+            CalculateDamage();
+            Debug.WriteLine($"Po wykonaniu SetMagic: {Damage} (rzut: {Roll})");
+        }
+
+        public void SetFlaming(bool isFlaming)
+        {
+            if (isFlaming)
+                FlamingDamage = FLAME_DAMAGE;
+            else
+                FlamingDamage = 0;
+
+            CalculateDamage();
+
+            Debug.WriteLine($"Po wykonaniu SetFlaming: {Damage} (rzut: {Roll})");
+        }
+    }
+}
